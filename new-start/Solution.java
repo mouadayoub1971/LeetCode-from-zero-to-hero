46a15b363d56724e9c8e99c4b2fd663dae51cc3c
mouad ayoub <mouadayoubmouadayoub@gmail.com>
2025-08-05 12:28:25 +0100

    167. Two Sum II - Input Array Is Sorted

--- new-start/Solution.java
+++ new-start/Solution.java
@@ -1,19 +1,18 @@
 import java.util.HashSet;
 
 public class Solution {
- public int lengthOfLongestSubstring(String s) {
-  int leftPointer = 0;
-  int maxLength = 0;
-  HashSet<Character> mySet = new HashSet<>();
-  for (int rightPointer = 0; rightPointer < s.length(); rightPointer++) {
-   Character current = s.charAt(rightPointer);
-   while (mySet.contains(current)) {
-    mySet.remove(s.charAt(leftPointer++));
+ public int[] twoSum(int[] numbers, int target) {
+  int rightPointer = 0;
+  int leftPointer = numbers.length - 1;
+  while (rightPointer < leftPointer) {
+   if (numbers[rightPointer] + numbers[leftPointer] == target) {
+    return new int[] {rightPointer, leftPointer};
+   } else if( rightPointer + leftPointer < target){
+    rightPointer++;
+   } else if( rightPointer + leftPointer > target){
+    leftPointer--;
    }
-   mySet.add(current);
-   maxLength = Math.max(maxLength, rightPointer - leftPointer + 1);
   }
- 
-  return maxLength;
+  return null; 
  }
 }
